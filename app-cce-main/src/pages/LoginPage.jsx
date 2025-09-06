8fcddea68ce83d0dc17e0a6c0f2da89e2d0aca7b
patrulla-360 <patrullas360@gmail.com>
2025-09-05 23:57:46 -0300

    asd

--- app-cce-main/src/pages/LoginPage.jsx
+++ app-cce-main/src/pages/LoginPage.jsx
@@ -23,7 +23,7 @@ export default function LoginPage() {
 
 
       const res = await fetch(
-  `https://apis-cce-all-main-997103170342.us-east1.run.app/api/usuario/${dni}`,
+  `http://127.0.0.1:4001/api/usuario/${dni}`,
   {
   method: 'GET',
   credentials: 'include',   // OBLIGATORIO para que iOS acepte Set-Cookie cross-site
@@ -57,6 +57,8 @@ export default function LoginPage() {
       } else if (rol === "Responsable de escuela") {
         navigate("/responsables");
 
+} else if (rol === "Responsable de carga de una escuela") {
+        navigate("/responsables/inicio");
 
 
       } else {
